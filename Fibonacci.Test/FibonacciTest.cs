6047322551984f11e19016ed4ba4871a054d1d11
Catharina <catharina_@hotmail.com.br>
2023-04-25 19:36:20 -0300

    initial commit

---
+++ Fibonacci.Test/FibonacciTest.cs
@@ -0,0 +1,21 @@
+﻿using Xunit;
+using FluentAssertions;
+
+namespace Fibonacci.Test
+{
+    public class FibonacciTest
+    {
+        [Theory]
+        [InlineData(1, true)]
+        [InlineData(3, true)]
+        [InlineData(4, false)]
+        public void Check_NumberIsFibonacci_WhenNumberIsValid(int number, bool expected)
+        {
+            var sut = new Fibonacci();
+
+            var result = sut.Check(number);
+
+            result.Should().Be(expected);
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci.cs
@@ -0,0 +1,19 @@
+﻿
+using System;
+
+namespace Fibonacci
+{
+    public class Fibonacci
+    {
+        public bool Check(int number)
+        {
+            //formula (5N^2 + 4) or (5N^2 - 4) needs to be perfect square -- 13
+            number = 5 * (number * number);
+            if(Math.Sqrt(number + 4) % 1 == 0 || Math.Sqrt(number - 4) % 1 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
